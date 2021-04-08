7b60b3319e47f348e78b639efd9046a526c5850c
Filipe Monteiro <F.Monteiro@kohnle05.local>
2021-04-08 23:38:10 +0200

    Add project files.

---
+++ SecondLibrary/CodeTracking.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    public class cCodeTracking
+    {
+        readonly IList<cCodeTrackingStruct> xItems = new List<cCodeTrackingStruct>();
+
+        public void Add(String Description, eRelation Relation = eRelation.ToFunction)
+        {
+            cCodeTrackingStruct xItem = new cCodeTrackingStruct
+            {
+                _Date = DateTime.Now,
+                _Description = Description,
+                _Relation = Relation,
+                _Method = cStackTrace.Get_MethodName(cStackTrace.eMethodDetail.WithClass, 2)
+            };
+            xItems.Add(xItem);
+
+        }
+
+        public String Display()
+        {
+            String xDisplay = "";
+            foreach (cCodeTrackingStruct xItem in xItems)
+            {
+                xDisplay += $"{xItem._Date}  {xItem._Method}  {xItem._Description}";
+                xDisplay += Environment.NewLine;
+            }
+            return xDisplay;
+        }
+
+        public void Clear()
+        {
+            xItems.Clear();
+        }
+    }
+
+
+    class cCodeTrackingStruct
+    {
+        public DateTime _Date { get; set; }
+        public String _Description { get; set; }
+        public eRelation _Relation { get; set; }
+        public String _Method { get; set; }
+    }
+
+    public enum eRelation
+    {
+        Absolute = 1,
+        ToFunction = 2,
+        ToPrevious = 3,
+        ToItem = 4
+    }
+
+}
---
+++ SecondLibrary/StackTrace.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    public class cStackTrace
+    {
+        public enum eMethodDetail
+        {
+            OnlyMethodName = 1,
+            WithClass = 2
+        }
+
+        public static string Get_MethodName(eMethodDetail MethodDetail, int Level = 1)
+        {
+            StackTrace st = new();
+            StackFrame sf = st.GetFrame(Level);
+
+            string xClassName = "";
+            string[] xSplit = sf.GetMethod().ReflectedType.ToString().Split('.', StringSplitOptions.RemoveEmptyEntries);
+            if (xSplit.Length >= 1)
+                xClassName = xSplit[1];
+
+            string xMethodName = sf.GetMethod().Name;
+
+            switch (MethodDetail)
+            {
+                case eMethodDetail.OnlyMethodName: { return xMethodName; }
+                case eMethodDetail.WithClass: { return xClassName + "." + xMethodName; }
+                default: { return null; }
+            }
+
+        }
+    }
+}
